f8776e8ca2b4c9871bab62818a2814302b3e7146
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-02-09 10:28:57 -0800

    last check on mobile version

--- app.js
+++ app.js
@@ -27,3 +27,35 @@ $(window).scroll(function () {
     )
 })
 
+const projects = $('#projects')
+// const myScrollFunc = (e) => {
+//     e.preventDefault()
+//     projects.hide()
+//     const y = $(window).scrollTop()
+//     console.log(y)
+//     if (y > 620) {
+
+//         projects.fadeIn()
+//     } else {
+//         //projects.delay(1000).fadeOut(500)
+//     }
+
+// }
+
+//$(window).on('scroll', myScrollFunc)
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $('.showElement').each(function (i) {
+            const bottomOfObject = $(this).position().top + $(this).outerHeight();
+            const bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+            if (bottomOfWindow > bottomOfObject) {
+                $(this).animate({ 'opacity': '1' }, 1500)
+            }
+        })
+    })
+
+
+})
+
